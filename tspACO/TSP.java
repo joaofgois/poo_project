e410430947c17f0efd43997ec43b0b33c121b4e0
joaofgois <92721998+joaofgois@users.noreply.github.com>
2023-06-16 00:23:09 +0100

    "merged" with class that contains the main

--- tspACO/TSP.java
+++ tspACO/TSP.java
@@ -1,30 +1,35 @@
 package tspACO;
 
 import sed.*;
+import graph.*;
 import antColony.*;
 import expRandom.*;
-import graph.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class TSP {
-    // private PriorityQueue<Cycle<Integer,Integer>> bestCycles;
-    protected float alpha, beta, delta;
-    protected float pheroLevel, eta, rho;
-    protected int nrPheroEvents, nrAntMoveEvents;
-    protected int antColonySize;
-    protected double finalInstant;
+
+    protected static float alpha, beta, delta;
+    protected static float pheroLevel, eta, rho;
+    protected static int nrPheroEvents, nrAntMoveEvents;
+    protected static int antColonySize, colonyNest;
+    protected static float finalInstant;
     protected WeightedGraph<Integer, Integer> graph;
-    protected float graphWeight; // falta isto
-    protected ISimulator simulator;
+    protected static float graphWeight; // falta isto
+    protected static ISimulator simulator;
+    protected ArrayList<Cycle<Integer, Integer>> hamiCycles;
 
     public static void main(String[] args) {
         WeightedGraph<Integer, Integer> grafo;
-        Scanner scanner;
         GraphCreator<Integer, Integer> context = new GraphCreator<Integer, Integer>();
+        Scanner scanner;
+        AntColony<Integer> antColony;
+        AntMoveEvent<Integer> antMoveEvent;
+
         int inputVertices = 0;
 
         System.out.println(args.length);
@@ -33,6 +38,16 @@ public class TSP {
             case 12:
                 if (args[0].equals("-r")) {
                     inputVertices = Integer.parseInt(args[1]);
+                    colonyNest = Integer.parseInt(args[3]);
+                    alpha = Float.parseFloat(args[4]);
+                    beta = Float.parseFloat(args[5]);
+                    delta = Float.parseFloat(args[6]);
+                    eta = Float.parseFloat(args[7]);
+                    rho = Float.parseFloat(args[8]);
+                    pheroLevel = Float.parseFloat(args[9]);
+                    antColonySize = Integer.parseInt(args[10]);
+                    finalInstant = Float.parseFloat(args[11]);
+
                     CreateGraphStrategy<Integer, Integer> generate;
                     generate = new RandomGraphStrategy(inputVertices);
 
@@ -47,16 +62,15 @@ public class TSP {
             // Input from terminal with file.
             case 2:
                 if (args[0].equals("-f")) {
-                	// Try to open the file
-                	try {
-                		scanner = new Scanner(new File(args[1]));
-                	} catch (FileNotFoundException e) {
-                		e.printStackTrace();
-                		return;
-                	}
-                    CreateGraphStrategy<Integer,Integer> readfile;
+                    // Try to open the file
+                    try {
+                        scanner = new Scanner(new File(args[1]));
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                    CreateGraphStrategy<Integer, Integer> readfile;
                     // Read input from file
-                    inputVertices = Integer.parseInt(scanner.next());
                     if (scanner.hasNextLine()) {
                         // skipping everything for now
                         scanner.nextLine();
@@ -77,7 +91,35 @@ public class TSP {
                 break;
         }
 
+        simulator = new Simulator(finalInstant);
         grafo = context.createGraph(inputVertices);
 
+        // Create the ant colony and the antMoveEvent
+        antColony = new AntColony<>(colonyNest);
+        for (int i = 0; i < antColonySize; i++) {
+            antColony.addAnt(i);
+            simulator.addEvPEC(new AntMoveEvent<Integer>(0, simulator, antColony, i, colonyNest, TSP.this));
+        }
+
+        // Iniciate the simulator
+        simulator.addEvPEC(new UpdateEvent<Integer>(0, simulator, TSP.this));
+        simulator.simulate();
+
+    }
+
+    public void storeCycle(ArrayList<Integer> cycle, int cycle_weight) {
+        // ArrayList1.equals(ArrayList2) == true
+        for (int i = 0; i < hamiCycles.size(); i++) {
+            if (hamiCycles.get(i).weight > cycle_weight) {
+                hamiCycles.add(i, new Cycle<Integer, Integer>(cycle, cycle_weight));
+                return;
+            }
+            if (hamiCycles.get(i).weight == cycle_weight) {
+                if (hamiCycles.get(i).cycle.equals(cycle)) {
+                    return;
+                }
+            }
+        }
+        hamiCycles.add(new Cycle<Integer, Integer>(cycle, cycle_weight));
     }
 }
--- tspACO/UpdateEvent.java
+++ tspACO/UpdateEvent.java
@@ -1,38 +1,45 @@
 package tspACO;
+
 import expRandom.*;
 import sed.Event;
 import sed.ISimulator;
-
 
 public class UpdateEvent<T> extends Event {
 	ExtRandom rand;
 	private int number;
 	ISimulator simulator;
+	TSP parent;
 
-	public UpdateEvent(float time, ISimulator simulator, TspACOSimulation<T, Integer> parent) {
+	public UpdateEvent(float time, ISimulator simulator, TSP parent) {
 		super(time);
 		this.rand = ExtRandom.getInstance();
 		this.number = 1;
 		this.simulator = simulator;
+		this.parent = parent;
 	}
 
 	@Override
 	protected void simulateEvent() {
-		//Observation number :
-		//Present instant: instant
-		//Number of move events: mevents
-		//Number of evaporation events: eevents
-		//Top candidate cycles: cycles
-		//Best Hamiltonian cycle: best
 		System.out.println("Observation " + number);
-		System.out.print("	Present instant " + time);
-		System.out.print("	Present instant " + time);
-		System.out.print("	Present instant " + time);
-		System.out.print("	Present instant " + time);
-		System.out.print("	Present instant " + time);
-		
+		System.out.println("	Present instant " + time);
+		System.out.println("	Number of move events: " + TSP.nrAntMoveEvents);
+		System.out.println("	Number of evaporation events: " + parent.nrPheroEvents);
+		// Print (up to 5) best candidate cycles:
+		if (parent.hamiCycles.size() > 0) {
+			System.out.printf("%-30s ", "Top candidate cycles:");
+		}
+		for (int i = 1; i < 6 && i < parent.hamiCycles.size(); i++) {
+			System.out.println("  " + parent.hamiCycles.get(i));
+		}
+		if (parent.hamiCycles.size() > 0) {
+			System.out.println("	Best Hamilton Cycle: " + parent.hamiCycles.get(0));
+		}
+		else {
+			System.out.println("	Best Hamilton Cycle: ");
+		}
+
 		number += 1;
-		time += simulator.getSimTime()/20;
+		time += simulator.getSimTime() / 20;
 		if (time < simulator.getSimTime()) {
 			simulator.addEvPEC(this);
 		}
